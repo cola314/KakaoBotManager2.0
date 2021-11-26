df87beeba941f37bf590d4fd867850548796e692
cola314 <ubj0314@naver.com>
2021-11-26 18:29:28 +0900

    [client] Seperate PopupBase from ResetPasswordPopup

---
+++ client/src/components/popups/PopupBase.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const PopupWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const PopupBlock = styled.div`
+  background: #f8f8f8;
+  width: 30rem;
+  padding: 1.5rem;
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+const PopupBase = ({ children }: Props) => {
+  return (
+    <PopupWrapper>
+      <PopupBlock>{children}</PopupBlock>
+    </PopupWrapper>
+  );
+};
+
+export default PopupBase;
--- client/src/components/popups/ResetPasswordPopup.tsx
+++ client/src/components/popups/ResetPasswordPopup.tsx
@@ -1,26 +1,8 @@
 import styled from 'styled-components';
 import Button from '../common/Button';
 import TextBoxInput from '../common/TextBoxInput';
+import PopupBase from './PopupBase';
 
-const PopupWrapper = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const PopupBlock = styled.div`
-  background: #f8f8f8;
-  width: 30rem;
-  padding: 1.5rem;
-  h2 {
-    font-size: 1.5rem;
-    margin-bottom: 2rem;
-  }
-`;
 const GridForm = styled.form`
   width: 100%;
   display: grid;
@@ -49,25 +31,23 @@ const ConfirmButton = styled(Button)`
 `;
 const ResetPasswordPopup = () => {
   return (
-    <PopupWrapper>
-      <PopupBlock>
-        <h2>비밀번호 재설정</h2>
-        <GridForm>
-          <label htmlFor="id">아이디</label>
-          <h4>admin</h4>
-          <label htmlFor="password">비밀번호 *</label>
-          <TextBoxInput placeholder="새 비밀번호" />
-          <label htmlFor="confirmPassword">비밀번호 *</label>
-          <TextBoxInput placeholder="비밀번호 재입력" />
-          <ErrorMessageBlock>
-            재설정 이후 변경이 불가능합니다 <br />
-            비밀번호 분실시 관리자를 통해서 비밀번호를 <br />
-            초기화해야합니다
-          </ErrorMessageBlock>
-          <ConfirmButton text="확인" />
-        </GridForm>
-      </PopupBlock>
-    </PopupWrapper>
+    <PopupBase>
+      <h2>비밀번호 재설정</h2>
+      <GridForm>
+        <label htmlFor="id">아이디</label>
+        <h4>admin</h4>
+        <label htmlFor="password">비밀번호 *</label>
+        <TextBoxInput placeholder="새 비밀번호" />
+        <label htmlFor="confirmPassword">비밀번호 *</label>
+        <TextBoxInput placeholder="비밀번호 재입력" />
+        <ErrorMessageBlock>
+          재설정 이후 변경이 불가능합니다 <br />
+          비밀번호 분실시 관리자를 통해서 비밀번호를 <br />
+          초기화해야합니다
+        </ErrorMessageBlock>
+        <ConfirmButton text="확인" />
+      </GridForm>
+    </PopupBase>
   );
 };
 
